51f8a6ad860dd12e571cf35b1eb29bd6c24ced18
nathanjh-28 <nathan@nathanjharris.com>
2022-06-20 10:30:00 -0500

    updated data js file and completed navbar component with styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <i class="fa-solid fa-earth my-logo" aria-hidden="true"></i>
+            <h3>My Travel Journal</h3>
+        </nav>
+    )
+}
